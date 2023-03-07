6880a412382d80ecefe4bb527ee63b27f15be8ba
fined000@naver.com <fined000@naver.com>
2023-03-07 23:22:02 +0900

    day 3 commit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import logger from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
-
 const app = express();
 
+app.use(logger('combined'));
+app.use("/", globalRouter);
+app.use("/video", videoRouter);
+app.use("/users", userRouter);
+
+
+const handleUser = (req, res) => res.send("Edit User");
 const handleListening = () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost: ${PORT}`)
 }
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening); //   PORT open.
+
